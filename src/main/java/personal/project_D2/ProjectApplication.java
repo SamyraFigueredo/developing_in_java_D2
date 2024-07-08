35a51708ca7a74c2481218ba3b8a74697957ecfb
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2024-07-08 11:47:50 -0300

    projeto Maven, mais dependencias,mais ajustes do pom.xml

---
+++ src/main/java/personal/project_D2/ProjectApplication.java
@@ -0,0 +1,12 @@
+package personal.project_D2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/personal/project_D2/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.project_D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
