fdf0cbbafaa5d3bdc223b0e8775042accf04519b
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2024-07-10 10:49:11 -0300

    adding item table

--- src/main/java/personal/project_D2/entities/Item.java
+++ src/main/java/personal/project_D2/entities/Item.java
@@ -2,22 +2,36 @@ package personal.project_D2.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "item")
 public class Item {
 
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
 	private String descricao;
 	private String categoria;
 	private int quantidade;
+
+	@Column(name = "data_validade")
 	private LocalDate validade;
 
 	public Item() {
 	}
 
-	public int getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -52,5 +66,4 @@ public class Item {
 	public void setValidade(LocalDate validade) {
 		this.validade = validade;
 	}
-
 }
